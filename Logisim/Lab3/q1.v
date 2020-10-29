e2e637d52684898340ef9210216d70378aa764be
BC-Li <251156266@qq.com>
2020-10-29 20:10:34 +0800

    Update Lab 3.

--- Logisim/Lab3/q1.v
+++ Logisim/Lab3/q1.v
@@ -1,3 +1,4 @@
+//  solution to q2
 module d_ff_tri(
     input clk,rst,d,
     output reg q
@@ -10,4 +11,42 @@ module d_ff_tri(
                 q<=d;
         end
 endmodule
-        
+//  solution to q3
+module Counter(
+    input clk,rst,
+    output reg [3:0] q
+);
+    always@(posedge clk)
+        begin
+            if(rst == 0)
+                q <= 4'b0;
+            else
+                q <= q + 4'b1;
+        end
+endmodule
+//solution to q4
+module LessenCounter(
+    input clk,rst,
+    output reg [3:0] q
+);
+    always@(posedge clk)
+        begin
+            if(rst == 0)
+                q <= 4'b1001;
+            else
+                q <= q - 4'b1;
+        end
+endmodule
+//solution to q5
+module LessenCounter_2(
+    input clk,rst,
+    output reg [3:0] q
+);
+    always@(posedge clk)
+        begin
+            if(rst == 1)
+                q <= 4'b1001;
+            else
+                q <= q - 4'b1;
+        end
+endmodule
