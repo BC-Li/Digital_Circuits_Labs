3b5a5103e1504e304e8baa57163aeb44e51cd0e3
BC-Li <251156266@qq.com>
2020-11-08 16:58:26 +0800

    Update Lab 5

---
+++ PracticeSet/Lab5/Lab5.srcs/sim_1/new/testbench_source.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/11/07 22:15:59
+// Design Name: 
+// Module Name: testbench_source
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench_source();
+    reg a, b;
+    initial clk = 0;
+endmodule
