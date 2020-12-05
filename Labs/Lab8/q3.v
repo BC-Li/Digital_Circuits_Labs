fc75f740ddc827ff1b4a2d8d318da0ba160eed44
BC-Li <251156266@qq.com>
2020-12-05 21:19:23 +0800

    Update Q3

--- Labs/Lab8/q3.v
+++ Labs/Lab8/q3.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2020/11/23 21:18:13
-// Design Name: 
-// Module Name: q1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 // solution to q3
 module q3(
@@ -102,21 +83,6 @@ module q3(
     
 endmodule
 
-// module jitter_clr(
-// input clk,
-// input button,
-// output button_clean
-// );
-// reg [3:0] cnt;
-// always@(posedge clk)
-// begin
-//  if(button==1'b0)
-//  cnt <= 4'h0;
-//  else if(cnt<4'h8)
-//  cnt <= cnt + 1'b1;
-// end
-// assign button_clean = cnt[3];
-// endmodule
 
 module signal_edge(
 input clk,
